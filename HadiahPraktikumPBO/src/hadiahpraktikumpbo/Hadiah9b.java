eba4bae44c2d649de12694247effbe839ab4ca28
RonaldoYusuf <90240652+ronaldoyusuf@users.noreply.github.com>
2021-11-17 09:49:43 +0800

    Add files via upload

---
+++ HadiahPraktikumPBO/src/hadiahpraktikumpbo/Hadiah9b.java
@@ -0,0 +1,39 @@
+package hadiahpraktikumpbo;
+
+import javax.swing.*;
+ import java.awt.event.*;
+public abstract class Hadiah9b implements ActionListener{
+
+
+private static void createAndShowGUI() {
+JFrame frame = new JFrame ("I am a JFrame"); frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE) ; 
+frame.setBounds (20, 30, 300, 100);
+ frame.getContentPane ().setLayout (null);
+ JButton butt=new JButton("click me");
+  frame.getContentPane () .add (butt);
+   butt.setBounds (20, 20, 200,20);
+
+Hadiah9b app = new Hadiah9b() {};
+app.label = new JLabel("Clicks = 1");
+ app. label.setBounds (20,40, 200, 20);
+  frame.getContentPane () .add (app. label);
+
+butt.addActionListener (app);
+ frame.setVisible(true);
+ }
+ 
+public void actionPerformed (ActionEvent e)
+{
+clickCount++; clickCount++; 
+label.setText ("Clicks = "+clickCount);
+}
+public static void main(String[] args) {
+SwingUtilities.invokeLater(new Runnable() {
+	 public void run() {
+createAndShowGUI();
+}
+});
+}
+int clickCount=1;
+ JLabel label;
+ }
